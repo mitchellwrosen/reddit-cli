66e2c32ec2ada0f2fb540877279be7022c075f2e
Mitchell Rosen <mitchellwrosen@gmail.com>
2016-01-21 15:08:30 -0800

    added forestCollapse

--- src/Brick/Widgets/Tree.hs
+++ src/Brick/Widgets/Tree.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE RankNTypes      #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RankNTypes          #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
 
 module Brick.Widgets.Tree
     ( Forest(..)
@@ -22,6 +23,7 @@ import Brick.Widgets.Core
 import Control.Lens hiding ((|>))
 import Data.List.NonEmpty (NonEmpty(..))
 import Data.Sequence      (Seq, (|>))
+import GHC.Exts
 
 import qualified Data.List.NonEmpty as NonEmpty
 import qualified Data.Sequence      as Seq
@@ -46,59 +48,59 @@ data Forest a = Forest
     }
 makeLenses ''Forest
 
--- | Construct a 'Forest, given a forest of 'Tree's.
+-- | Construct a 'Forest', given a forest of 'Tree's.
 forest :: Name -> [Tree a] -> Forest a
 forest name trees = Forest name trees (zipper (dfForest mempty trees))
-  where
-    -- Depth-first traversal of a tree, given the tree, and the path
-    -- taken thus far. Does not traverse past collapsed nodes, but does
-    -- include them.
-    --
-    -- For example,
-    --
-    --                   o0
-    --     dfTree []    / \
-    --                 o1   o2
-    --                /  \
-    --               x0   o3
-    --              / \
-    --             o4  o5
-    --
-    --     == [[], [0], [0,0], [0,1], [1]]
-    --         ^    ^     ^      ^     ^
-    --         o0   o1    x0     o3    o4
-    --
-    -- where 'o' represents a non-collapsed tree, and
-    --       'x' represents a collapsed tree
-    --
-    dfTree :: Seq Int -> Tree a -> [Seq Int]
-    dfTree is (Tree _ _ True) = [is]
-    dfTree is (Tree _ ns _) = is : dfForest is ns
 
-    -- Like 'dfTree', but for a forest of trees.
-    --
-    -- For example,
-    --
-    --                        o0          o5
-    --     dfForest [] [     / \         / \    ]
-    --                      o1  o2      o6  o7
-    --                     / \             /
-    --                    o3  o4    ,     x0
-    --                                   /
-    --                                  o8
-    --
-    --     == [[0], [0,0], [0,0,0], [0,0,1], [0,1], [1], [1,0], [1,1], [1,1,0]]
-    --          ^     ^       ^        ^       ^     ^     ^      ^       ^
-    --          o0    o1      o3       o4      o2    o5    o6     o7      x0
-    --
-    -- where 'o' represents a non-collapsed tree, and
-    --       'x' represents a collapsed tree
-    --
-    dfForest :: Seq Int -> [Tree a] -> [Seq Int]
-    dfForest is ns = concatMap go (zip [0..] ns)
-      where
-        go :: (Int, Tree a) -> [Seq Int]
-        go (i, n) = dfTree (is |> i) n
+-- Depth-first traversal of a tree, given the tree, and the path
+-- taken thus far. Does not traverse past collapsed nodes, but does
+-- include them.
+--
+-- For example,
+--
+--                   o0
+--     dfTree []    / \
+--                 o1   o2
+--                /  \
+--               x0   o3
+--              / \
+--             o4  o5
+--
+--     == [[], [0], [0,0], [0,1], [1]]
+--         ^    ^     ^      ^     ^
+--         o0   o1    x0     o3    o4
+--
+-- where 'o' represents a non-collapsed tree, and
+--       'x' represents a collapsed tree
+--
+dfTree :: Seq Int -> Tree a -> [Seq Int]
+dfTree is (Tree _ _ True) = [is]
+dfTree is (Tree _ ns _) = is : dfForest is ns
+
+-- Like 'dfTree', but for a forest of trees.
+--
+-- For example,
+--
+--                        o0          o5
+--     dfForest [] [     / \         / \    ]
+--                      o1  o2      o6  o7
+--                     / \             /
+--                    o3  o4    ,     x0
+--                                   /
+--                                  o8
+--
+--     == [[0], [0,0], [0,0,0], [0,0,1], [0,1], [1], [1,0], [1,1], [1,1,0]]
+--          ^     ^       ^        ^       ^     ^     ^      ^       ^
+--          o0    o1      o3       o4      o2    o5    o6     o7      x0
+--
+-- where 'o' represents a non-collapsed tree, and
+--       'x' represents a collapsed tree
+--
+dfForest :: Seq Int -> [Tree a] -> [Seq Int]
+dfForest is ns = concatMap go (zip [0..] ns)
+  where
+    go :: (Int, Tree a) -> [Seq Int]
+    go (i, n) = dfTree (is |> i) n
 
 -- | Render a forest, given an element rendering function. Children of collapsed
 -- elements will not be rendered.
@@ -125,9 +127,20 @@ forest name trees = Forest name trees (zipper (dfForest mempty trees))
 --            then w0
 --            else w0 <=> padLeft (Pad 1) w1
 
--- | Collapse (or uncollapse) the selected node of a tree.
--- treeCollapse :: Tree a -> Tree a
--- treeCollapse t = t & treeNodes %~ nodesCollapse (t^.treeSelected)
+
+-- | Collapse (or uncollapse) the selected 'Tree' of a 'Forest'.
+forestCollapse :: forall a. Forest a -> Forest a
+forestCollapse (Forest name trees Nothing) = Forest name trees Nothing
+forestCollapse (Forest name trees (Just z)) =
+    let selected :: Traversal' [Tree a] (Tree a)
+        selected = treeIndex (toList (zpeek z))
+
+        trees' :: [Tree a]
+        trees' = over (selected . treeCollapsed) not trees
+    in forest name trees'
+
+-- forestCollapse :: Forest a -> Forest a
+-- forestCollapse f = t & treeNodes %~ nodesCollapse (t^.treeSelected)
 
 -- nodesCollapse :: [Int] -> [Node a] -> [Node a]
 -- nodesCollapse ns = over (nodeIndex ns . nodeCollapsed) not
@@ -148,10 +161,10 @@ forest name trees = Forest name trees (zipper (dfForest mempty trees))
 -- clamp i j n = max i (min j n)
 
 -- Invariant: list of ints is not empty
--- treeIndex :: [Int] -> Traversal' [Node a] (Node a)
--- nodeIndex [n] = ix n
--- nodeIndex (n:ns) = ix n . nodeChildren . nodeIndex ns
---
+treeIndex :: [Int] -> Traversal' [Tree a] (Tree a)
+treeIndex [n] = ix n
+treeIndex (n:ns) = ix n . treeChildren . treeIndex ns
+
 -- idxDown :: [Int] -> [Int]
 -- idxDown []     = []
 -- idxDown (_:xs) = xs
